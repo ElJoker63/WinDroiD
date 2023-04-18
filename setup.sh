d5c0c0aed25a4eadc88f4c1663841e295b4572bd
ElJoker63 <eljoker630@gmail.com>
2023-04-18 14:31:48 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-echo "WinDroiD v1.7.1"
-echo "latest version contributors : AKPR2007,Exanoff12"
+echo "WinDroiD v1.7.2"
+echo "latest version contributors : AKPR2007,Exanoff12,ElJoker63"
 echo "preparing to start process"
 sleep 6s
 clear
@@ -14,6 +14,28 @@ if [ -n "$TERMUX_VERSION" ]; then
 		echo "termux version is supported. continuing setup"
 		sleep 3s
 		clear
+		echo "updating and upgrading packages"
+		sleep 3s
+		yes | apt update && yes | apt upgrade
+		clear
+		echo "installating/updating wget"
+		sleep 3s
+		pkg install wget -y
+		clear
+		echo "installating/updating unzip"
+		sleep 3s
+		pkg install unzip -y
+		clear
+		echo "enabling/updating x11-repo"
+		sleep 3s
+		pkg install x11-repo -y
+		clear
+		echo "installating/updating qemu-system-i386"
+		sleep 3s
+		pkg install qemu-system-i386-headless -y
+		clear
+		echo "setting up storage access"
+		sleep 3s
 		if [ -d "storage" ]; then
 			echo "internal storage access already given. skipping step"
 		else
@@ -35,10 +57,10 @@ if [ -n "$TERMUX_VERSION" ]; then
 		clear
 		echo "downloading files from external server"
 		sleep 3s
-		wget -O w7.zip "http://192.168.137.1/WinDroiD/w7.zip"
+		wget -O w7.zip "http://192.168.137.1/WinDroiD/w7.zip?dl=0"
 		clear
 		echo "finalising setup"
-		sleep 60s
+		sleep 3s
 		unzip w7.zip
 		rm w7.zip
 		wget -O win7 "https://raw.githubusercontent.com/ElJoker63/WinDroiD/main/start.sh"
